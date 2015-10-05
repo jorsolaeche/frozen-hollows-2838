abc4ca5a1cfb96e00f7feb75c37d539c6aec6f02
jorsolaeche <jorsolaeche@gmail.com>
2015-10-05 15:36:26 -0300

    new

--- czuonline/wsgi.py
+++ czuonline/wsgi.py
@@ -1,5 +1,5 @@
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
  
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "czuonline.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "czuonline.settings")
 application = Cling(get_wsgi_application())
