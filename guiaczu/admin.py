be2cb6a9fbd61ed05db45a55114618af3ae0561f
jorsolaeche <jorsolaeche@gmail.com>
2015-10-06 17:09:32 -0300

    adminfb

--- guiaczu/admin.py
+++ guiaczu/admin.py
@@ -5,7 +5,7 @@ from .models import Empresa, Rubro
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
 	list_display = ('razonsocial', 'created', 'modified', 'user',)
-	fieldsets = (None, {'fields': ( ('razonsocial', 'logo'), 'rubro', ('telefono', 'direccion', 'latitud', 'longitud',), ('user',) ) }),
+	fieldsets = (None, {'fields': ( ('razonsocial', 'logo'), 'rubro','facebook_id', ('telefono', 'direccion', 'latitud', 'longitud',), ('user',) ) }),
 	list_filter = ['rubro',]
 	search_fields = ('razonsocial',)
 
