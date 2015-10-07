c51abf0c0a40d3161483be9f33ae1421969d95d9
jorsolaeche <jorsolaeche@gmail.com>
2015-10-07 11:16:36 -0300

    new

--- czuonline/urls.py
+++ czuonline/urls.py
@@ -11,7 +11,4 @@ urlpatterns = [
     #Admin
     url(r'^admin/', include(admin.site.urls)),
 ]
-urlpatterns += patterns('',
- (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
- )
 
