97a325fe0973e2ec5f0022d3b335e5a18352f8f5
andres <andres@debian.debian>
2015-10-13 10:33:32 -0300

    agregado de la url media en el urls.py

--- czuonline/urls.py
+++ czuonline/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     #Inicio
 	url(r'^', include('guiaczu.urls', namespace='guiaczu')),
     #Archivos/imagenes
-    url(r'^documents/(.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT}),
+    url(r'^media/(.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT}),
     #Admin
     url(r'^admin/', include(admin.site.urls)),
 ]
